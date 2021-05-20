194458edd51a1806295c2868373df5c2c45f073d
DianChen-Korff <79322059+DianChen-Korff@users.noreply.github.com>
2021-05-20 19:08:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const myAge = 17;
+const isFemale = true;
+const diverStatus = "bob";
+
+if (myAge > 18) {
+
+    console.log ("je bent ouder dan 18 en mag binnenkomen!");
+
+} else if (myAge == 18) {
+
+    console.log ("je bent 18 en je mag binnenkomen!");
+
+} else {
+
+    console.log ("je bent jonger dan 18 en mag helaas NIET binnenkomen!");
+
+}
+
+if (isFemale) {
+
+    console.log ("je bent een vrouw!");
+
+} else {
+
+    console.log ("je bent een man!");
+    
+}
+
+if (diverStatus=== "bob") {
+
+    console.log ("jij bent bob en mag WEL rijden!")
+
+} else {
+
+    console.log ("jij bent GEEN bob en mag dus NIET rijden!");
+    
+}
